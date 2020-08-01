ee6801081f1fdcb5c0a3a40e19e65c671ffc5594
Rohit <rv021561@gmail.com>
2020-08-01 16:16:19 +0530

    Mongoose starts working

--- app.js
+++ app.js
@@ -7,14 +7,43 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/todolistDB" , {useNewUrlParser:true, useUnifiedTopology: true});
 
+const workSchema = mongoose.Schema({
+    name : {
+        type : String
+    }
+});
+
+const Work = mongoose.model("work",workSchema);
+
+const work1 = new Work({
+    name : "Welcome to To do"
+});
+
+const work2 = new Work({
+    name : "hit + to add an item"
+});
+
+const work3 = new Work({
+    name : "<-- to delete an item"
+});
+
+var defaultList = [work1, work2, work3];
+
+Work.insertMany(defaultList, function(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("The values are inserted !");
+    }
+})
+
 
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.set("view engine","ejs");
 
 app.use(express.static(__dirname+"/public"))
-
-var item = [];
 
 app.get("/" ,function(req, res){
     var option = {
@@ -25,7 +54,9 @@ app.get("/" ,function(req, res){
     }
     var dateObj = new Date();
     var date = dateObj.toLocaleDateString("en-US",option);
-    res.render("index", {item : item, date : date});
+    Work.find({} ,function(err, arrayOfItems){
+        res.render("index", {item : arrayOfItems , date : date});
+    });
 });
 
 app.post("/", function(req , res){
